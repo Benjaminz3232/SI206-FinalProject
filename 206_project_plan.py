d7f0c2c97dc3ad275d2305b3615d2e8e1a42cf9b
Benjamin3232 <bzeffer@umich.edu>
2017-04-09 21:42:41 -0400

    making changes

--- 206_project_plan.py
+++ 206_project_plan.py
@@ -1,25 +1,20 @@
-## Your name:
-## The option you've chosen:
+## Your name: Benjamin Zeffer
+## The option you've chosen: 2
 
 # Put import statements you expect to need here!
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+import unittest
+import requests
+import tweepy
+import twitter_info # as always when dealing with twitter because proper authentication is needed!
+import json
+import sqlite3
+import itertools
+import collections
 
 # Write your test cases here.
 
 
+
 ## Remember to invoke all your tests...
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
