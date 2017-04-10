f02fdab314afa81b730692ea3333ea32f49e5e47
Benjamin3232 <bzeffer@umich.edu>
2017-04-09 22:10:09 -0400

    adding more test cases

--- 206_project_plan.py
+++ 206_project_plan.py
@@ -2,48 +2,85 @@
 ## The option you've chosen: 2
 
 # Put import statements you expect to need here!
-import unittest
-import requests
+import unittest # for testing
+import requests 
 import tweepy
 import twitter_info # as always when dealing with twitter because proper authentication is needed!
 import json
 import sqlite3
-import itertools
-import collections
+import itertools # for generators and list comprehension
+import collections # used for containers and Counter
 
 # Write your test cases here.
 
 class MovieTestCases(unittest.TestCase):
-	
+
     base_url = "http://www.omdbapi.com/"
-    params = {}
-    params["t"] = "mean girls"
-    r = requests.get(base_url, params=params)
-    data = json.loads(r.text)
+    parameters = {}
+    parameters["t"] = "mean girls"
+    response = requests.get(base_url, params=parameters)
+    data = json.loads(response.text)
 
+    # This will be testing the movie title
     def test_movie_title(self):
-        movie = Movie(data)
-        self.assertEqual(movie.title, "Mean Girls")
+        m = Movie(data)
+        self.assertEqual(m.title, "Mean Girls")
 
+    # This will be testing the __str__ method for the class Movie and it's "output"
+    def test_movie_str(self):
+        m = Movie(data)
+        self.assertEqual(m.__str__(), "Mean Girls, directed by Mark Waters")
+
+    # This will be testing the plot of the movie
+    def test_movie_plot(self):
+        m = Movie(data)
+        self.assertEqual(m.plot, "Cady Heron is a hit with The Plastics, the A-list girl clique at her new school, until she makes the mistake of falling for Aaron Samuels, the ex-boyfriend of alpha Plastic Regina George.")
+
+    # This will be testin gthe movie's release date
     def test_movie_released(self):
-    	movie = Movie(data)
-    	self.assertEqual(movie.released, "30 Apr 2004")
+    	m = Movie(data)
+    	self.assertEqual(m.released, "30 Apr 2004")
 
+    # This will be testing the actors
     def test_movie_actors(self):
-        movie = Movie(data)
-        self.assertEqual(movie.actors, "Lindsay Lohan, Rachel McAdams, Tina Fey, Tim Meadows"))
+        m = Movie(data)
+        self.assertEqual(m.actors, ["Lindsay Lohan", "Rachel McAdams", "Tina Fey", "Tim Meadows"])
+		self.assertEqual(len(m.actors), 4)
+		self.assertEqual(type(m.actors), list)
 
 	def test_movie_IMDBrating(self):
-		movie = Movie(data)
-		self.assertEqual(movie.IMDBrating, "84%")
+		m = Movie(data)
+		self.assertEqual(m.IMDBrating, "84%")
 
-    def test_movie_plot(self):
-        movie = Movie(data)
-        self.assertEqual(movie.plot, "Cady Heron is a hit with The Plastics, the A-list girl clique at her new school, until she makes the mistake of falling for Aaron Samuels, the ex-boyfriend of alpha Plastic Regina George.")
 
-    def test_movie_str(self):
-        movie = Movie(data)
-        self.assertEqual(movie.__str__(), "Mean Girls, directed by Mark Waters")
+
+
+
+class DatabaseTests(unittest.TestCase):
+
+    def test_movie_db_columns1(self):
+        conn = sqlite3.connect('final_project.db')
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM Movies')
+        result = cur.fetchall()
+        self.assertEqual(len(result[0]), 8)
+        conn.close()
+
+    def test_users_db_columns2(self):
+        conn = sqlite3.connect('final_project.db')
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM Users')
+        result = cur.fetchall()
+        self.assertEqual(len(result[0]), 4)
+        conn.close()
+
+    def test_tweets_db_columns3(self):
+        conn = sqlite3.connect('final_project.db')
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM Tweets')
+        result = cur.fetchall()
+        self.assertEqual(len(result[0]), 6)
+        conn.close()
 
 
 ## Remember to invoke all your tests...
